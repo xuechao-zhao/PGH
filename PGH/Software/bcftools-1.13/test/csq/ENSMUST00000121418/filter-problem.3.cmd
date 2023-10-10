5e097d0a27a5b53239e00e02c2d8650bf054ef2e
赵学超 <zhao387@biox.org.cn>
2023-10-10 09:04:46 +0800

    modify

--- PGH/Software/bcftools-1.13/test/csq/ENSMUST00000121418/filter-problem.3.cmd
+++
@@ -1,1 +0,0 @@
-{bin}/bcftools csq -f ENSMUST00000121418.fa -g ENSMUST00000121418.gff filter-problem.vcf -i 'POS!=25500' --phase m | ../sort-csq | {bin}/bcftools query -f'%POS\t%REF\t%ALT\t%BCSQ\n'
--- PGH/Software/bcftools-1.13/test/csq/ENSMUST00000133836/filter-problem.1.cmd
+++
@@ -1,1 +0,0 @@
-{bin}/bcftools csq -f ENSMUST00000133836.fa -g ENSMUST00000133836.gff filter-problem.vcf -i 'FILTER="PASS"' --phase m | ../sort-csq | {bin}/bcftools query -f'%POS\t%REF\t%ALT\t%BCSQ\n'
